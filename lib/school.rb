6f3cba4b32c5a8eed770c23fe5e2bb178686788d
tatiana.mojica <tatiana.mojica@bustle.com>
2018-11-16 19:58:27 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,22 +1,24 @@
 # code here!
-# require 'pry'
+require 'pry'
 
 class School 
   def initialize(school_name)
-  @school_name = school_name
-  @roster = {}
+    @school_name = school_name
+    @roster = {}
   end
   
   def roster 
     @roster 
+    # or you can create attribute reader up top 
   end 
   
   def add_student(name, grade)
-      @roster[grade] ||= []
+    @roster[grade] ||= []
      
-      # the array has to first be empty/created in order to add items to that array 
-      # if what is in [grade] is defined and evaluates to truthy, then the right hand side of the operator is not evaluated, and no assignment takes place
-      @roster[grade] << name
+    # the array has to first be empty/created in order to add items to that array 
+    # if what is in [grade] is defined and evaluates to truthy, then the right hand side of the operator is not evaluated, and no assignment takes place
+    # ?? Can you swap name and grade for key > values? no b/c roster is the name 
+    @roster[grade] << name
     # grade is the key you have created and name is the value
   end
 
@@ -26,11 +28,12 @@ class School
   end
 
   def sort
-    @roster.each do |grade, name|
-      @roster[grade] = name.sort
-      # we are pulling the grade roster and then sorting it by name 
-    end 
-  end
+    @roster.each_with_object({}) {|(grade, names), sorted_roster| sorted_roster[grade] = names.sort}
+    
+    # each_with_object is changing a hash to an array (grade,names) then creating a sorted roster based by grade as the key and sorted names for the values. 
+  end 
+    
 end
+
 
    
